d7dfa17ddcee1a892c848c1e0502e828e773cad6
Keegan Sheedy <keegan@leansentry.com>
2019-11-21 13:42:59 -0500

    Add project files.

---
+++ SessionLockingApp/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SessionLockingApp.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RedirectToAccountPage()
+        {
+            // Save info to session from querystring 
+            Session["Name"] = Request.QueryString["Name"];
+            Session["Age"] = Request.QueryString["Age"];
+
+            return View("Login");
+        }
+    }
+}
---
+++ SessionLockingApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SessionLockingApp.Controllers
+{
+    [SessionState(System.Web.SessionState.SessionStateBehavior.ReadOnly)]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Initialize session state 
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            //Gather account info 
+            Thread.Sleep(10000);
+            string name = "Keegan";
+            int age = 22;
+
+            return RedirectToAction("RedirectToAccountPage", "Account", new {Name = name, Age = age });
+        }
+    }
+}
